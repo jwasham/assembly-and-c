8d216b824b904b6ec34885cef12a31b5c6330ad5
jwasham <ubuntu@ip-10-0-1-180.ec2.internal>
2018-11-26 04:26:37 +0000

    Adds comments for strlen

--- ch02/strlen.asm
+++ ch02/strlen.asm
@@ -2,19 +2,19 @@ global _start
 
 section .data
 
-test_string: db "abcdef", 0
+test_string: db "abcdef", 0    ; the string to check for length, terminated with null
 
 section .text
 
 strlen:
-    xor rax, rax
+    xor rax, rax               ; zero out rax - we'll increment from 0
 
 .loop:
-    cmp byte[rdi + rax], 0
+    cmp byte[rdi + rax], 0     ; look at string's byte, indexed starting at rdi
  
     je .end
 
-    inc rax
+    inc rax                    ; increment rax: increments str len size, and our rdi index
     jmp .loop
 
 .end:
@@ -23,8 +23,8 @@ strlen:
 _start:
     mov rdi, test_string
     call strlen
-    mov rdi, rax
-
+    mov rdi, rax               ; from command line, determine str len by calling echo $?
+                               ; $? returns exit code from last command
     mov rax, 60
     syscall
 
